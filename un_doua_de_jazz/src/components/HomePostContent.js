07f1e8dad09b2427dfbb8e445c94ab79f3c1f127
Alice <alice.invernizzi@outlook.fr>
2023-03-16 14:58:36 +0100

    Modif de la page d'accueil

--- un_doua_de_jazz/src/components/HomePostContent.js
+++ un_doua_de_jazz/src/components/HomePostContent.js
@@ -15,10 +15,9 @@ function HomeContent(nb) {
     
 return (
     <div id="homeContent">
-        <p id="venir">MERCI !</p>
+        <p id="venir">Le festival du Doua De Jazz fait son retour du 12 au 20 Octobre 2023 pour sa 30e édition ! </p>
         <hr id="smallhr"/>
-        <p id="patienter"><span>Merci aux bénévoles, aux partenaires et surtout aux festivaliers !<br/>
-        Actuellement nous formons les rangs pour la 30ème qui s'annonce folle !</span></p>
+        <p id="patienter"><span>En attendant n’hésite pas à regarder les prochains évènements que nous organisons, ou à redécouvrir nos anciennes éditions</span></p>
         <div id="gridPatienter">
             <div id="nosArtistes">
 
