f13aa299e8f5b5df086efa823fbff068dc39877f
Alice <alice.invernizzi@outlook.fr>
2023-04-13 21:38:04 +0200

    Modif Tremplin

--- un_doua_de_jazz/src/components/Tremplin.js
+++ un_doua_de_jazz/src/components/Tremplin.js
@@ -21,16 +21,18 @@ return (
 
             </div>
             <div id="tremplinText">
+            <p>Ouverture des inscriptions !</p>
             <p>Le Doua de Jazz organise tous les ans un concert-tremplin afin de découvrir et mettre
 en avant des artistes émergents. Trois artistes/groupes de talent sélectionnés sont
-ainsi invités à se produire à La Clé de Voûte, un club de jazz réputé de Lyon.</p>
-            <p>Cette année, rendez-vous le 1er Juin à la clé de voûte pour le tremplin du Doua De Jazz. Les
+ainsi invités à se produire à La Clef de Voûte, un club de jazz réputé de Lyon.</p>
+            <p>Cette année, rendez-vous le 1er Juin à 20h30 à la Clef de Voûte pour le tremplin du Doua De Jazz. Les
 artistes sélectionnés seront annoncés quelques semaines avant le concert, alors restez
 connectés !</p>
-            <p>Ce tremplin permet au groupe vainqueur de se produire à notre festival et d’obtenir
-une somme de 1000 euros. Le deuxième prix s’élève quant à lui à 500 euros.</p>
+            <p>Ce tremplin permet au groupe vainqueur de se produire à notre festival lors d'une première partie pour un concert, et également d'obtenir
+une somme de 1000 euros d'accompagnement. Le deuxième prix s’élève quant à lui à 500 euros.</p>
 <p>Pour participer, envoyez nous votre candidature contenant une courte présentation
-ainsi qu’une vidéo ou un audio par mail à <a href="mailto:communication@undouadejazz.com">communication@undouadejazz.com</a></p>
+ainsi qu’une vidéo ou un extrait audio par mail à <a href="mailto:communication@undouadejazz.com">communication@undouadejazz.com</a></p>
+            <p>3 finalistes seront ainsi sélectionnés pour jouer devant un jury professionnel !</p>
             </div>
             <hr/>
 
