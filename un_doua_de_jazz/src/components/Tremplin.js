8545daf0637b2dcd5109e24682c014c30b257352
Alice <alice.invernizzi@outlook.fr>
2023-05-07 12:21:32 +0200

    Maj tremplin

--- un_doua_de_jazz/src/components/Tremplin.js
+++ un_doua_de_jazz/src/components/Tremplin.js
@@ -29,7 +29,7 @@ ainsi invités à se produire à La Clef de Voûte, un club de jazz réputé de 
 une somme de 1000 euros d'accompagnement. Le deuxième prix s’élève quant à lui à 500 euros.</p>
 <p>Pour participer, envoyez-nous votre candidature contenant une courte présentation
 ainsi qu’une vidéo ou un extrait audio par mail à <a href="mailto:communication@undouadejazz.com">communication@undouadejazz.com</a> avant le 5 mai.</p>
-            <p>Cette année, rendez-vous le 1er Juin à 20h30; l'annonce des 3 finalistes sélectionnés pour jouer devant le jury sera bientôt sur nos réseaux, alors restés connectés !</p>
+            <p>Cette année, rendez-vous le 1er Juin à 20h30; l'annonce des 3 finalistes sélectionnés pour jouer devant le jury sera bientôt sur nos réseaux, alors restez connectés !</p>
             </div>
             <hr/>
 
