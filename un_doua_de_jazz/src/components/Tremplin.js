debc97fcddd44937aae248a84fbd1e7dc13eebcd
Alice <alice.invernizzi@outlook.fr>
2023-05-07 12:16:43 +0200

    Màj fin tremplin

--- un_doua_de_jazz/src/components/Tremplin.js
+++ un_doua_de_jazz/src/components/Tremplin.js
@@ -25,14 +25,11 @@ return (
             <p>Le Doua de Jazz organise tous les ans un concert-tremplin afin de découvrir et mettre
 en avant des artistes émergents. Trois artistes/groupes de talent sélectionnés sont
 ainsi invités à se produire à La Clef de Voûte, un club de jazz réputé de Lyon.</p>
-            <p>Cette année, rendez-vous le 1er Juin à 20h30. Les
-artistes sélectionnés seront annoncés quelques semaines avant le concert, alors restez
-connectés !</p>
             <p>Ce tremplin permet au groupe vainqueur de se produire à notre festival lors d'une première partie pour un concert, et également d'obtenir
 une somme de 1000 euros d'accompagnement. Le deuxième prix s’élève quant à lui à 500 euros.</p>
 <p>Pour participer, envoyez-nous votre candidature contenant une courte présentation
-ainsi qu’une vidéo ou un extrait audio par mail à <a href="mailto:communication@undouadejazz.com">communication@undouadejazz.com</a></p>
-            <p>3 finalistes seront ainsi sélectionnés pour jouer devant un jury professionnel !</p>
+ainsi qu’une vidéo ou un extrait audio par mail à <a href="mailto:communication@undouadejazz.com">communication@undouadejazz.com</a> avant le 5 mai.</p>
+            <p>Cette année, rendez-vous le 1er Juin à 20h30; l'annonce des 3 finalistes sélectionnés pour jouer devant le jury sera bientôt sur nos réseaux, alors restés connectés !</p>
             </div>
             <hr/>
 
